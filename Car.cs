aeb809b0dee366b873f9fbaa1c40103db5190f1a
jhwil90 <31808102+jhwil90@users.noreply.github.com>
2017-10-11 01:44:30 -0400

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Car
+    {
+        //Members
+        protected string carMake;
+        private string carModel;
+        private int carYear;
+        private double carPrice;
+        
+        // property
+        public string Make
+        {
+            set { Make = value; }
+            get { return Make; }
+
+        }
+
+        public string Model
+        {
+            set { Model = value; }
+            get { return Model; }
+        }
+
+        public int Year
+        {
+            set { Year = value; }
+            get { return Year; }
+        }
+
+        public double Price
+        {
+            set { Price = value; }
+            get { return Price; }
+        }
+
+
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Year = 0;
+            Price = 0;
+            
+        }
+
+        public Car(string CarMake, string CarModel, int CarYear, double CarPrice)
+        {
+            CarMake = Make;
+            CarModel = Model;
+            CarYear = Year;
+            CarPrice = Price;
+            
+
+
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine(Make.PadRight(8, ' ') + "\t" + Model.PadRight(8, ' ') + "\t" + Year.ToString().PadRight(8, ' ') + "\t" + "$" + Price.ToString("N2"));
+        }
+    }
+}
